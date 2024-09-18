12ad5134cc8839c6f1212fdfdb97757763593648
j-pcgoncalves <joao_goncalves_7@hotmail.com>
2024-09-18 18:53:12 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <>
+      <p>Configuração Inicial Apicolmeias: React (TypeScript) + Vite</p>
+      <p>Framework Tailwind CSS: <span className="text-green-500">ON</span></p>
+    </>
+  )
+}
+
+export default App
