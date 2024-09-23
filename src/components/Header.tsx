180cdeedc9775e273112abc3c7f4e747ab1859ab
j-pcgoncalves <joao_goncalves_7@hotmail.com>
2024-09-23 21:13:02 +0100

    better ui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <div className="bg-white flex justify-between py-5 px-20 items-center">
+        <div className="bg-white flex justify-between py-5 px-36 items-center">
             <h1 className="font-dancing font-extrabold text-6xl text-primary">Apicolmeias</h1>
 
             <ul className="flex gap-16 text-lg items-center">
