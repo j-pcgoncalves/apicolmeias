9e184784db72986783070f59f7b15ac2df742b40
j-pcgoncalves <joao_goncalves_7@hotmail.com>
2025-02-02 18:19:28 +0000

    create an style hero section (Home component)

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,21 @@
+import { NavLink } from "react-router-dom";
+import heroBg from "../../assets/hero_bg.jpg";
+
+const Home = () => {
+    return (
+        <>
+            <div style={{ backgroundImage: `url(${heroBg})` }} className="hero-section">
+                <div className="ml-5 md:ml-20">
+                        <p className="hero-main-text-1">COMÉRCIO</p>
+                        <p className="hero-main-text-2">APÍCOLA</p>
+
+                    <NavLink to="/produtos" className="hero-btn">
+                        Ver Produtos
+                    </NavLink>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Home;
--- src/main.tsx
+++ src/main.tsx
@@ -9,12 +9,17 @@ import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import App from './App.tsx'
 import './index.css'
 import Contactos from './components/contactos/Contactos.tsx';
+import Home from './components/home/Home.tsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
+      {
+        path: "/",
+        element: <Home />
+      },
       {
         path: "/contactos",
         element: <Contactos />
